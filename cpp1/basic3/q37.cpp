d1eaf92066289ac3d97d0d4358e657ca4dbd14f0
nothinggh <git@github.com:nothinggh/AI-BS.git>
2026-07-30 09:43:18 +0900

    122

---
+++ cpp1/basic3/q37.cpp
@@ -0,0 +1,37 @@
+// Q37) C++로 사칙연산 프로그램을 만드세요.
+
+#include <iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+int multiple(int a, int b)
+{
+    return a * b;
+}
+
+double divide(int a, int b)
+{
+    return (double)a / b;
+}
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+
+    cout << "plus결과는 : " << add(a, b) << "입니다." << endl;
+    cout << "sub결과는 : " << sub(a, b) << "입니다." << endl;
+    cout << "multiple결과는 : " << multiple(a, b) << "입니다." << endl;
+    cout << "divide결과는 : " << divide(a, b) << "입니다." << endl;
+
+    return 0; // 객체지향 1
+}
