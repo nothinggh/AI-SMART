159b1ef5a513514bb2081cdb1f366c57050b6e2e
nothinggh <git@github.com:nothinggh/AI-BS.git>
2026-07-30 11:52:41 +0900

    0730

--- cpp/basic4/pure_virtual_test.cpp
+++ cpp/basic4/pure_virtual_test.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-class Shape
-{ // 순수 가상 함수가 1개라도 있으면 추상클래스가 된다.
+class Shape // 순수 가상 함수가 1개라도 있으면 추상클래스가 된다.
+{ 
 public:
     virtual void draw() = 0; // 순수 가상 함수
 };
@@ -18,5 +18,5 @@ int main()
     // Shape shape;
     Circle Circle;
 
-    return 0;
+   return 0;
 }
